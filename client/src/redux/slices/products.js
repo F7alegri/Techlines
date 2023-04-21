058328a9ef9a9380eaad5271cdf36b00a8291cd1
Federico Alegria <federico.alegria0049211@gmail.com>
2023-04-21 17:14:09 -0600

    add cart to item and remove

--- client/src/redux/slices/products.js
+++ client/src/redux/slices/products.js
@@ -1,31 +1,31 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const initialState = {
-    loading: false,
-    error: null,
-    products: [],
-}
+	loading: false,
+	error: null,
+	products: [],
+};
 
-export const productSlice = createSlice({
-    name: 'products',
-    initialState,
-    reducers: {
-        setLoading: (state) => {
-            state.loading = true;
-        },
-        setProducts: (state,{payload}) => {
-            state.loading = false;
-            state.error = null;
-            state.products = payload;
-        },
-        setError: (state, {payload}) => {
-            state.error = payload;
-            state.loading = false;
-        },
-    }
+export const productsSlice = createSlice({
+	name: 'products',
+	initialState,
+	reducers: {
+		setLoading: (state) => {
+			state.loading = true;
+		},
+		setProducts: (state, { payload }) => {
+			state.loading = false;
+			state.error = null;
+			state.products = payload;
+		},
+		setError: (state, { payload }) => {
+			state.error = payload;
+			state.loading = false;
+		},
+	},
 });
 
-export const {setLoading, setError, setProducts} = productSlice.actions;
-export default productSlice.reducer;
+export const { setLoading, setError, setProducts } = productsSlice.actions;
+export default productsSlice.reducer;
 
-export const productSelector = (state) => state.products;
+export const productsSelector = (state) => state.products;
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,10 +4,22 @@ import Product from '../models/Product.js';
 const productRoutes = express.Router();
 
 const getProducts = async (req, res) => {
-    const products = await Product.find({});
-    res.json(products)
+  const products = await Product.find({});
+  res.json(products);
+};
+
+const getProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found.');
+  }
 };
 
 productRoutes.route('/').get(getProducts);
+productRoutes.route('/:id').get(getProduct);
 
 export default productRoutes;
